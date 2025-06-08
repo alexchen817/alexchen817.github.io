37cc2b02b28cc1913bee41dabef3a0a882aea2d0
Alex Chen <alexanderchen817@gmail.com>
2025-06-07 23:22:31 -0700

    added tools anchor and made anchors navigate to tools tab and fix typo in nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ export default function NavBar() {
             <a href='#'>Home</a>
             <a href='#'>About</a>
             <a href='#'>Experience</a>
-            <a href='#'>Tools/Experience</a>
+            <a href='#tools'>Tools/Experience</a>
             <a href='#'>Projects</a>
             <a href='#'>Awards</a>
         </nav>
